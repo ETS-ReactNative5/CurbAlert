df0da2d1c9afb09efee58f4c74b758087cc3dd4e
Christopher Neal <christopher.m.neal@gmail.com>
2022-03-10 04:32:06 -0800

    change 'Loading...' to logo image

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -102,8 +102,12 @@ function ItemList({navigation, route}) {
   const checkIfLoading = () => {
     if (isLoading === true || itemList.empty === true) {
       return (
-        <View>
-          <Text style={styles.titleText}>Loading...</Text>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            source={require('./../assets/logo.png')}
+            opacity={0.8}
+            style={{width: 250, height: 250}}
+          />
         </View>
       );
     } else {
