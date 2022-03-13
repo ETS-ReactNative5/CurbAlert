bb650a351f566d16be0b269671ba7fe5b6ba74b0
Christopher Neal <christopher.m.neal@gmail.com>
2022-03-13 15:29:43 -0700

    move getDistance function to utils

---
+++ src/utils/getDistance.js
@@ -0,0 +1,21 @@
+// FROM https://geodatasource.com/developers/javascript
+export const getDistance = (lat1, lon1, lat2, lon2) => {
+  if (lat1 === lat2 && lon1 === lon2) {
+    return 0;
+  } else {
+    var radlat1 = (Math.PI * lat1) / 180;
+    var radlat2 = (Math.PI * lat2) / 180;
+    var theta = lon1 - lon2;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    const distInFeet = dist * 60 * 1.1515 * 1.609344 * 3280.84;
+    return Math.floor(distInFeet);
+  }
+};
